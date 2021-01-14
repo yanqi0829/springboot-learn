893d053cbf19b8a0fa76f2677659c67c8860cb73
wangyq <610300317@qq.com>
2021-01-14 14:53:19 +0800

    Initial commit

---
+++ src/test/java/com/spring/project/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
