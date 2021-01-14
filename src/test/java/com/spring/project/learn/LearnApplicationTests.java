10dff1cf90639b51dece0ba926363d4130f3ad24
wangyq <610300317@qq.com>
2021-01-14 15:50:49 +0800

    测试yml配置文件

--- src/test/java/com/spring/project/learn/LearnApplicationTests.java
+++ src/test/java/com/spring/project/learn/LearnApplicationTests.java
@@ -1,13 +1,18 @@
 package com.spring.project.learn;
 
+import com.spring.project.learn.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LearnApplicationTests {
+    @Autowired
+    Person person;
 
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
