157939a71ac1239a300698161b224e66678d910e
wangyq <610300317@qq.com>
2021-01-14 16:28:57 +0800

    2.1.3  @ConfigurationProperties 与@Value

--- src/test/java/com/spring/project/learn/LearnApplicationTests.java
+++ src/test/java/com/spring/project/learn/LearnApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spring.project.learn;
 
+import com.spring.project.learn.bean.Cat;
 import com.spring.project.learn.bean.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LearnApplicationTests {
     @Autowired
     Person person;
+    @Autowired
+    Cat cat;
 
     @Test
     void contextLoads() {
         System.out.println(person);
+        System.out.println(cat);
     }
-
 }
